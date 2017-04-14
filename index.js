85376a4a996738260c1af90c23219c18acd3c104
Geoff Dutton <g.dutton@gmail.com>
2017-04-14 09:59:36 -0500

    added Jest tests and replaced jshint with eslint

--- index.js
+++ index.js
@@ -1,56 +1,54 @@
-'use strict';
-var gutil = require('gulp-util');
-var map = require('map-stream');
-var spawn = require('win-spawn');
-var dargs = require('dargs');
+
+var gutil = require('gulp-util')
+var map = require('map-stream')
+var spawn = require('win-spawn')
+var dargs = require('dargs')
 
 module.exports = function (options) {
-	options = options || {};
-	var passedArgs = dargs(options, ['bundleExec']);
-	var bundleExec = options.bundleExec;
+	options = options || {}
+	var passedArgs = dargs(options, ['bundleExec'])
+	var bundleExec = options.bundleExec
 
 	return map(function (file, cb) {
-
 		if (file.isStream()) {
-			return cb(new gutil.PluginError('gulp-jekyll', 'Streaming not supported'));
+			return cb(new gutil.PluginError('gulp-jekyll', 'Streaming not supported'))
 		}
 
 		var args = [
 			'jekyll',
 			'build'
-		].concat(passedArgs);
+		].concat(passedArgs)
 
 		if (bundleExec) {
-			args.unshift('bundle', 'exec');
+			args.unshift('bundle', 'exec')
 		}
 
-		var cp = spawn(args.shift(), args);
+		var cp = spawn(args.shift(), args)
 
 		cp.on('error', function (err) {
-			return cb(new gutil.PluginError('gulp-jekyll', err));
-		});
+			return cb(new gutil.PluginError('gulp-jekyll', err))
+		})
 
-		var errors = '';
-		cp.stderr.setEncoding('utf8');
+		var errors = ''
+		cp.stderr.setEncoding('utf8')
 		cp.stderr.on('data', function (data) {
-			errors += data;
-		});
+			errors += data
+		})
 
 		cp.on('close', function (code) {
 			if (code === 127) {
-				return cb(new gutil.PluginError('gulp-jekyll', 'You need to have Ruby and Jekyll installed and in your PATH for this task to work.'));
+				return cb(new gutil.PluginError('gulp-jekyll', 'You need to have Ruby and Jekyll installed and in your PATH for this task to work.'))
 			}
 
 			if (errors) {
-				return cb(new gutil.PluginError('gulp-jekyll', '\n' + errors.replace('Use --trace for backtrace.\n', '')));
+				return cb(new gutil.PluginError('gulp-jekyll', '\n' + errors.replace('Use --trace for backtrace.\n', '')))
 			}
 
 			if (code > 0) {
-				return cb(new gutil.PluginError('gulp-jekyll', 'Exited with error code ' + code));
+				return cb(new gutil.PluginError('gulp-jekyll', 'Exited with error code ' + code))
 			}
 
-			cb(null, null);
-
-		});
-	});
-};
+			cb(null, null)
+		})
+	})
+}
---
+++ tests/index.spec.js
@@ -0,0 +1,140 @@
+
+var gutil = require('gulp-util')
+var spawn = require('win-spawn')
+jest.mock('win-spawn')
+var jekyll = require('../')
+
+var fakeStream
+
+beforeEach(() => {
+	fakeStream = {
+		on: jest.fn((ev, cb) => {
+			if (ev === 'close') {
+				cb(0)
+			}
+		}),
+		stderr: {
+			setEncoding: jest.fn(),
+			on: jest.fn((ev, cb) => {
+
+			})
+		}
+	}
+
+	spawn.mockReturnValue(fakeStream)
+})
+
+test('throws stream not supported error', () => {
+	return new Promise((resolve) => {
+		// returns a read/write stream
+		var stream = jekyll()
+		stream.on('error', (err) => {
+			expect(err).toEqual(new gutil.PluginError('gulp-jekyll', 'Streaming not supported'))
+			resolve()
+		})
+		stream.write({ isStream: () => true })
+	})
+})
+
+test('catches errors from spawned process', () => {
+	fakeStream.on = jest.fn((ev, cb) => {
+		cb(new gutil.PluginError('gulp-jekyll', new Error('some spawn error')))
+	})
+	return new Promise((resolve) => {
+		var stream = jekyll()
+		stream.on('error', (err) => {
+			expect(err).toEqual(new gutil.PluginError('gulp-jekyll', new Error('some spawn error')))
+			expect(spawn).toHaveBeenCalledWith('jekyll', ['build'])
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+	})
+})
+
+test('sets encoding to utf8', () => {
+	return new Promise((resolve) => {
+		var stream = jekyll()
+		stream.on('close', () => {
+			expect(fakeStream.stderr.setEncoding).toHaveBeenCalledWith('utf8')
+			expect(spawn).toHaveBeenCalledWith('jekyll', ['build'])
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
+
+test('allows for bundle exec', () => {
+	return new Promise((resolve) => {
+		var stream = jekyll({ bundleExec: true })
+		stream.on('close', () => {
+			expect(fakeStream.stderr.setEncoding).toHaveBeenCalledWith('utf8')
+			expect(spawn).toHaveBeenCalledWith('bundle', ['exec', 'jekyll', 'build'])
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
+
+test('allows further jekyll commands ot be passed', () => {
+	return new Promise((resolve) => {
+		var stream = jekyll({ something: true })
+		stream.on('close', () => {
+			expect(fakeStream.stderr.setEncoding).toHaveBeenCalledWith('utf8')
+			expect(spawn).toHaveBeenCalledWith('jekyll', ['build', '--something'])
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
+
+test('checks if jekyll is installed', () => {
+	fakeStream.on = jest.fn((ev, cb) => {
+		if (ev === 'close') {
+			cb(127)
+		}
+	})
+	return new Promise((resolve) => {
+		var stream = jekyll()
+		stream.on('error', (err) => {
+			expect(err).toEqual(new gutil.PluginError('gulp-jekyll', 'You need to have Ruby and Jekyll installed and in your PATH for this task to work.'))
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
+
+test('throws an error if spawn stderr outputs', () => {
+	fakeStream.stderr.on = jest.fn((ev, cb) => {
+		cb('OMG Something went COMPLETELY WRONG!!!Use --trace for backtrace.\n')
+	})
+	return new Promise((resolve) => {
+		var stream = jekyll()
+		stream.on('error', (err) => {
+			expect(err).toEqual(new gutil.PluginError('gulp-jekyll', '\nOMG Something went COMPLETELY WRONG!!!'))
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
+
+test('checks errors out if spawn returns non 0 code', () => {
+	fakeStream.on = jest.fn((ev, cb) => {
+		if (ev === 'close') {
+			cb(1)
+		}
+	})
+	return new Promise((resolve) => {
+		var stream = jekyll()
+		stream.on('error', (err) => {
+			expect(err).toEqual(new gutil.PluginError('gulp-jekyll', 'Exited with error code 1'))
+			resolve()
+		})
+		stream.write({ isStream: () => false })
+		stream.destroy()
+	})
+})
